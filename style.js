6d4408e411227000682b9c30892da88f5d6f0de5
Radha Verma <radver12345@gmail.com>
2023-08-16 17:28:51 +0530

    Initial commit

---
+++ style.js
@@ -0,0 +1,7 @@
+ const typed = new Typed('.multiple-text',{
+    strings: ["", 'Software Developer', 'CODER', 'Java Full Stack Developer', 'FrontEnd Developer', 'Programmer'],
+    typeSpeed: 100,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop: true
+ });
